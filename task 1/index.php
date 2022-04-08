958289fa3a0aa6d300bafabc6de2e94850dc5811
giorgioshots <giorgio@shots.it>
2022-04-08 18:13:10 +0200

    typo

--- task 1/index.php
+++ task 1/index.php
@@ -4,7 +4,7 @@ include("all-dependencies.php");
 $emailBody = "
     Ciao __USERNAME__,
     sappiamo che hai comprato il prodotto __PRODUCTNAME__ in data __ORDERDATE__.
-    Facci sapere come lo hai trovato: scrivi una recensione per ottenre subito il 10% di sconto!
+    Facci sapere come lo hai trovato: scrivi una recensione per ottenere subito il 10% di sconto!
 ";
 
 sendMail( 'giorgio@shots.it', 'Giorgio', $emailBody );
