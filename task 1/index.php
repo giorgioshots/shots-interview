fb95e67b8c1cab791078cae777059a71e56ad92e
Giorgio <giorgio@shots.it>
2022-04-08 17:28:19 +0200

    task 1

---
+++ task 1/index.php
@@ -0,0 +1,12 @@
+<?php
+include("all-dependencies.php");
+
+$emailBody = "
+    Ciao __USERNAME__,
+    sappiamo che hai comprato il prodotto __PRODUCTNAME__ in data __ORDERDATE__.
+    Facci sapere come lo hai trovato: scrivi una recensione per ottenre subito il 10% di sconto!
+";
+
+sendMail( 'giorgio@shots.it', 'Giorgio', $emailBody );
+
+?>
